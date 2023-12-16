2583aadf975468c17a2b678b5f5cf4536487abf1
dhfanny <dhfannyy>
2023-12-16 11:25:00 +0700

    first commit

---
+++ app/views/home/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="jumbotron py-4 px-5">
+        <h1 class="display-4">Selamat Datang!</h1>
+        <hr class="my-4">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos id, incidunt quam maxime quos repudiandae cumque suscipit laboriosam, est molestias neque tempora vero rem similique perspiciatis adipisci ex quaerat sed?</p>
+    </div>
+</div>
